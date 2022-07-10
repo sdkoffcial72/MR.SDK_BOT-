554e59d5a99be28657bfe69c7d09d6de993606e2
sdkoffcial72 <105613695+sdkoffcial72@users.noreply.github.com>
2022-07-10 22:07:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
+global.vcardowner = ['94740031178'] //ur owner number
 global.ownername = "❓ꪶмя⿻ѕ∂кꫂ❔" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
